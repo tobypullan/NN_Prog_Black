3c290ceba093c3b904397d23209c8a4a0962b575
Johnny Ho <jdho2006@gmail.com>
2025-04-26 19:30:13 +0100

    Added DenseLayer implementation

--- layers.py
+++ layers.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from matrix import Matrix
 
 
 class Layer(ABC):
@@ -30,3 +31,36 @@ class Layer(ABC):
         Return the number of neurons in the layer
         """
         pass
+    
+    
+class DenseLayer(Layer):
+    def __init__(self, neurons):
+        self._neurons = neurons
+        self.batch_size = None
+        self.weights = None
+        self.biases = None
+        self.in_matrix = None
+        self.dw = None
+        self.db = None
+        
+    def setup(self, input_neurons, batch_size):
+        self.weights = Matrix.random((self._neurons, input_neurons))
+        self.biases = Matrix.random((self._neurons, 1))
+        self.batch_size = batch_size
+        
+    def forward(self, in_matrix):
+        self.in_matrix = in_matrix
+        return self.weights @ in_matrix + self.biases
+    
+    def backward(self, gradients, learning_rate):
+        self.dw = gradients @ self.in_matrix.transpose() * 1 / self.batch_size
+        self.db = gradients.sum(axis=0) * 1 / self.batch_size
+        
+        self.weights = self.weights - self.dw * learning_rate
+        self.biases = self.biases - self.db * learning_rate
+        
+        return self.weights.transpose() @ gradients
+    
+    @property
+    def neurons(self):
+        return self._neurons
