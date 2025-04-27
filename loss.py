d009b0dd1926e9bba76476f963de44baa65e7756
tobypullan <toby@pullan.org>
2025-04-27 19:58:06 +0100

    MSE loss function!!!!

---
+++ loss.py
@@ -0,0 +1,12 @@
+from matrix import Matrix
+class MSE: # mean square error loss function
+    def __call__(self, prediction, y):
+        """
+        perform MSE calculation on prediction and y (both matrix objects)
+        """
+        return (prediction - y).map(lambda x: x**2)
+    def derivative(self, prediction, y):
+        """
+        used in backpropagation, derivative of loss
+        """
+        return (prediction - y) * 2
---
+++ lossInterface.py
@@ -0,0 +1,11 @@
+class MSE: # mean square error loss function
+    def __call__(self, prediction, y):
+        """
+        perform MSE calculation on prediction and y (both matrix objects)
+        """
+        pass
+    def derivative(self, prediction, y):
+        """
+        used in backpropagation, derivative of loss
+        """
+        pass
